1e3a859461a71214cd6d173b8b1424b1a885886b
aslamalu89 <azlmalu3@gmail.com>
2023-07-21 08:48:37 -0700

    second commit

--- lib/task3.dart
+++ lib/task3.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
 void main(){
   var list1 = [];
-  print ("enter a name");
-  var name = stdin.readLinesync();
-  var b ={ "name": name};
-
+  print("enter the name:");
+  var name = stdin.readLineSync();
+  var b ={"name": name};
+  print(b);
+  print("enter email:");
+  var email = stdin.readLineSync();
+  var c ={"email":email};
+  print(c);
+  print("enter address:");
+  var address = stdin.readLineSync();
+  var e ={"address":address};
+  list1.addAll([b, c,e]);
+  print(list1);
+}
